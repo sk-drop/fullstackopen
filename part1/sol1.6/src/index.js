24188700f6d41d2df1d60fa19c98f9983f0499be
sk-drop <sasha-kaun@gmx.de>
2020-09-28 22:54:43 +0200

    adding solution for 1.9

--- part1/sol1.6/src/index.js
+++ part1/sol1.6/src/index.js
@@ -1,13 +1,42 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (props) => {
+  return(
+    <div>
+      <h2>statistics</h2>
+      <p>good: {props.good}</p>
+      <p>neutral: {props.neutral}</p>
+      <p>bad: {props.bad}</p>
+      <p>all: {props.all}</p>
+      <p>average: {props.average}</p>
+      <p>positive: {props.positive}%</p>
+    </div>
+  )
+}
+
 
 const App = () => {
   // save clicks of each button to own state
-
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const reset = () => {
+    return(
+      setGood(0),
+      setNeutral(0),
+      setBad(0)
+    )
+  }
+
+  const feedback = ((good+bad+neutral) > 0)
+
+  const show = () => {
+    if (feedback) {return(<Statistics good = {good} neutral = {neutral} bad ={bad} all = {good+bad+neutral}
+      average = {(good-bad)/(good+bad)} positive = {(good/(good+neutral+bad))*100}/>)}
+    else {return(<p>no feedback given</p>)}
+  }
 
   return (
     <div>
@@ -15,13 +44,8 @@ const App = () => {
       <button onClick = {() => setGood(good+1)}> good </button>
       <button onClick = {() => setNeutral(neutral+1)}> neutral </button>
       <button onClick = {() => setBad(bad+1)}> bad </button>
-      <h2>statistics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {good+neutral+bad}</p>
-      <p>average: {(good+neutral+bad)/2}</p>
-      <p>positive: {(good/(neutral+bad+good))*100}%</p>
+      <button onClick = {() => reset()}> reset </button>
+      <p>{show()}</p>
     </div>
   )
 }
