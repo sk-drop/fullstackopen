eea40c61b0234f891f5366abc8ce6d41b1000bf7
sk-drop <sasha-kaun@gmx.de>
2020-09-28 22:15:48 +0200

    adding solution for 1.7

--- part1/sol1.6/src/index.js
+++ part1/sol1.6/src/index.js
@@ -19,6 +19,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {good+neutral+bad}</p>
+      <p>average: {(good+neutral+bad)/2}</p>
+      <p>positive: {(good/(neutral+bad+good))*100}%</p>
     </div>
   )
 }
