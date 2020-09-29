592cbe1d9dc79f523f81358799eee06960590a3a
sk-drop <sasha-kaun@gmx.de>
2020-09-29 12:30:16 +0200

    adding final solution for 1.*

--- part1/sol1.12/src/index.js
+++ part1/sol1.12/src/index.js
@@ -1,22 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
-
-  const rand = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
-  }
-
-  return (
-    <div>
-      <p>{props.anecdotes[selected]}</p>
-      <button onClick={rand}>another one</button>
-    </div>
-  )
-}
-
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -25,6 +9,39 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
+
+const votes = new Array(anecdotes.length).fill(0)
+
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const rand = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const vote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    votes[selected] = copy[selected]
+  }
+
+  var maxVotes = votes.indexOf(Math.max(...votes))
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={rand}>another one</button>
+      <button onClick={vote}>vote</button>
+      <h2>Anecdote with most votes</h2>
+      <p>{props.anecdotes[maxVotes]}</p>
+      <p>has {votes[maxVotes]} votes</p>
+      
+    </div>
+  )
+}
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
